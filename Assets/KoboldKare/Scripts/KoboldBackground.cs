0dbe650c1f444b937f1c9f862e9762178774eef9
naelstrof <naelstrof@gmail.com>
2021-09-17 16:56:29 -0600

    added some bg art for the menu

--- Assets/KoboldKare/Scripts/KoboldBackground.cs
+++ Assets/KoboldKare/Scripts/KoboldBackground.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class KoboldBackground : MonoBehaviour {
     public Animator animator;
     public Image kobold;
+    public Image trees;
+    public Image mountain;
     IEnumerator KoboldBackgroundRoutine() {
         while(isActiveAndEnabled) {
             yield return new WaitForSecondsRealtime(Random.Range(0f,8f));
@@ -21,12 +23,16 @@ public class KoboldBackground : MonoBehaviour {
     }
     void Update() {
         RectTransform rk = kobold.GetComponent<RectTransform>();
+        RectTransform tk = trees.GetComponent<RectTransform>();
+        RectTransform mk = mountain.GetComponent<RectTransform>();
         rk.localScale = new Vector3(1f,1f+Mathf.Sin(Time.unscaledTime*2.2f)*0.03f,1f);
 
         Vector2 mousePos = Mouse.current.position.ReadValue();
         Vector2 offset = mousePos - new Vector2(Screen.width, Screen.height)*0.5f;
         float distance01 = Mathf.Clamp01(new Vector2(offset.x*2f/Screen.width, offset.y/Screen.height).magnitude*2f);
 
-        rk.anchoredPosition = offset * Easing.Quadratic.Out(distance01)*0.2f;
+        rk.anchoredPosition = -offset * Easing.Quadratic.Out(distance01)*0.1f;
+        tk.anchoredPosition = -offset * Easing.Quadratic.Out(distance01)*0.05f;
+        mk.anchoredPosition = -offset * Easing.Quadratic.Out(distance01)*0.025f;
     }
 }

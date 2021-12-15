6f2dca8c3b5f3eae26a904c1b9ccb9528bbc9b0b
Reiikz <raltcrafter@gmail.com>
2021-12-15 00:44:04 -0300

    Fix frame not loading

--- Assets/ThirdParty/Reiikz/Scripts/LoadCustomParts.cs
+++ Assets/ThirdParty/Reiikz/Scripts/LoadCustomParts.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LoadCustomParts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+ 
+    private static bool IsLoaded(string name)
     {
-        SceneManager.LoadScene("HeadPat", LoadSceneMode.Additive);
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        yield return new WaitForSeconds(2.5f);
+        if(IsLoaded("MainMap")){
+            if(!IsLoaded("HeadPat")) {
+                SceneManager.LoadScene("HeadPat", LoadSceneMode.Additive);
+                Debug.Log("PENIS!");
+            }
+        }
     }
 
     // Update is called once per frame

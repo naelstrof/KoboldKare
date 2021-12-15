87dcea90f3e1a7928d6631f353bbeb0d4e137c3e
Firgof <firgof.umbra@gmail.com>
2021-12-15 12:39:09 -0500

    DayNightCycle affects sun's intensity during eclipse as well, restored orig. rain values

--- Assets/KoboldKare/Scripts/SunManager.cs
+++ Assets/KoboldKare/Scripts/SunManager.cs
@@ -6,10 +6,16 @@ using UnityEngine.Rendering;
 public class SunManager : MonoBehaviour {
     public Material skyboxMaterial;
     public Volume postProcessNight;
+    public Light sun;
+    private float origSunIntensity;
+    void Start(){
+        origSunIntensity = sun.intensity;
+    }
     void Update() {
         skyboxMaterial.SetFloat("_Eclipse", DayNightCycle.instance.time01);
         float zeroAdd = Mathf.Clamp01(1f - DayNightCycle.instance.time01 * 25f);
         float oneAdd = Mathf.Clamp01(1f-Mathf.Abs(DayNightCycle.instance.time01 - 1f) * 25f);
         postProcessNight.weight = zeroAdd+oneAdd;
+        sun.intensity = origSunIntensity-(origSunIntensity*(zeroAdd+oneAdd));
     }
 }

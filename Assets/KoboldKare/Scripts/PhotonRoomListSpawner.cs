22082abeb49e5c9b919a67245937c5a1c8252c1f
Firgof <firgof.umbra@gmail.com>
2022-01-09 14:25:18 -0500

    Contract materials for purchasables; rude hookups for new buildings

--- Assets/KoboldKare/Scripts/PhotonRoomListSpawner.cs
+++ Assets/KoboldKare/Scripts/PhotonRoomListSpawner.cs
@@ -1,23 +1,46 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class PhotonRoomListSpawner : MonoBehaviourPunCallbacks, ILobbyCallbacks {
     public GameObject roomPrefab;
     public GameObject hideOnRoomsFound;
     private List<GameObject> roomPrefabs = new List<GameObject>();
+    public List<RoomInfo> curRoomList;
+
+    public override void OnConnectedToMaster(){
+        base.OnConnectedToMaster();
+        Debug.Log("PhotonRoomListSpawner :: Connected to master");
+    }
+    public override void OnLeftLobby(){
+        ClearRoomList();
+        Debug.Log("[PhotonRoomListSpawner] :: Player left lobby");
+    }
+    
+
     public override void OnRoomListUpdate(List<RoomInfo> roomList) {
+        base.OnRoomListUpdate(roomList); // Perform default expected behavior
+        Debug.Log("[PhotonRoomListSpawner] :: Got room list update from master server");
         hideOnRoomsFound.SetActive(true);
-        foreach(GameObject g in roomPrefabs) {
-            Destroy(g);
+        ClearRoomList();
+        //Build new list to refresh UI on
+        foreach(RoomInfo info in roomList) {    
+            if(info.RemovedFromList == true || info.IsOpen == false){
+                ClearRoomFromList(info);
+                continue;
+            }
+            else{
+                if(!curRoomList.Contains(info)) //Only add it in if we don't have it already
+                    curRoomList.Add(info);
+            }
         }
-        roomPrefabs.Clear();
-        foreach(RoomInfo info in roomList) {
-            hideOnRoomsFound.SetActive(false);
+
+        //Build UI from current room list
+        foreach (RoomInfo info in curRoomList){
             GameObject room = GameObject.Instantiate(roomPrefab, this.transform);
             roomPrefabs.Add(room);
             room.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = info.Name;
@@ -29,9 +52,19 @@ public class PhotonRoomListSpawner : MonoBehaviourPunCallbacks, ILobbyCallbacks 
                 NetworkManager.instance.JoinMatch(info.Name);
             });
         }
+
+        if(roomList.Count > 0) //Only call once
+            hideOnRoomsFound.SetActive(false);
+    }
+    private void ClearRoomFromList(RoomInfo room){
+        curRoomList.Remove(room);
     }
 
-    public void UpdateList(){
-        //Update
+    private void ClearRoomList(){
+        foreach(GameObject g in roomPrefabs) {
+            Destroy(g);
+        }
+        roomPrefabs.Clear();
+        curRoomList.Clear();
     }
 }

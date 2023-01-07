00335bf8b1b3bf0fdaba8e6ba521d862b7c0eb5a
Razielsoulshadow <Razielsoulshadow@yahoo.com>
2023-01-06 19:33:51 -0800

    Add Intermediate Stage to Menu Hide, Alter Button
    Adds an intermediate stage to the Menu Hide feature, removing only the
    non-functional aspects. This allows to see the kobold better AND keep
    editing them without having to unhide and rehide the menu repeatedly.
    
    Altered button to fit new functionality, making it match the color
    scheme of the other buttons and instead displaying a "indicator light"
    with the current stage of hiding next to it instead. Button is now
    smaller to fit, does unfortunately fail to line up as well text-wise.
    
    Also, moved the reset all kobold settings button to the bottom. It just
    feels better there personally. In future may also add additional spacing
    between the button and the last option somehow, and enlarge the title of
    the menu (and maybe change it from "Kobold" to "Default Kobold" or
    "Kobold Options")

--- Assets/KoboldKare/Scripts/HideAllViews.cs
+++ Assets/KoboldKare/Scripts/HideAllViews.cs
@@ -1,12 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class HideAllViews : MonoBehaviour {
+public class HideAllViews : MonoBehaviour 
+{
     public List<GameObject> views = new List<GameObject>();
-    public void HideThem() {
-        foreach(var view in views) {
-            view.SetActive(false);
-        }
-    }
+
+	[SerializeField]
+	private GameObject header, panel, multiplayer, indicator;
+	private CanvasGroup canvas;
+	private Image panelImage, multiplayerImage, indicatorColor;
+	private int step = 0;
+
+
+	public void Start()
+	{
+		panelImage = panel.GetComponent<Image>();
+		multiplayerImage = multiplayer.GetComponent<Image>();
+		indicatorColor = indicator.GetComponent<Image>();
+		canvas = multiplayer.GetComponent<CanvasGroup>();
+	}
+
+
+	public void HideThem()
+	{
+		foreach (var view in views)
+		{
+			view.SetActive(false);
+		}
+		header.SetActive(true);
+		panelImage.enabled = true;
+		multiplayerImage.enabled = true;
+		canvas.alpha = 1f;
+		canvas.interactable = true;
+		indicatorColor.color = new Color32(200, 0, 0, 255);
+		step = 1;
+	}
+
+
+	public void HideKoboldMenu()
+	{
+		//Debug.Log("start " + step);
+		switch (step)
+		{
+			default:
+				header.SetActive(true);
+				panelImage.enabled = true;
+				multiplayerImage.enabled = true;
+				canvas.alpha = 1;
+				canvas.interactable = true;
+				indicatorColor.color = new Color32(200, 0, 0, 255);
+				step=0;
+				break;
+			case 1:
+				header.SetActive(false);
+				panelImage.enabled = false;
+				multiplayerImage.enabled = false;
+				indicatorColor.color = new Color32(200, 200, 0, 255);
+				break;
+			case 2:
+				canvas.alpha = 0;
+				canvas.interactable = false;
+				indicatorColor.color = new Color32(0, 200, 0, 255);
+				break;
+		}
+		step++;
+		//Debug.Log("end " + step);
+
+	}
+
+	
 }

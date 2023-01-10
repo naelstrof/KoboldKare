a56cac64db230342a9533e849b246da23c4e44b1
Naelstrof <naelstrof@gmail.com>
2023-01-09 22:19:15 -0700

    Maybe fixed cursor lock on scene load

--- Assets/KoboldKare/Scripts/PlayerPossession.cs
+++ Assets/KoboldKare/Scripts/PlayerPossession.cs
@@ -58,6 +58,10 @@ public class PlayerPossession : MonoBehaviourPun {
     private InputSystemUIInputModule inputModule;
     public UnityScriptableSettings.SettingFloat mouseSensitivity;
     public void OnPause() {
+        if (!isActiveAndEnabled) {
+            return;
+        }
+
         if (GetEquipmentUI()) {
             SetEquipmentUI(false);
             return;

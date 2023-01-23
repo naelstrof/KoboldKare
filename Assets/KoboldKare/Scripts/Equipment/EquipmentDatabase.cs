5d8b94bff1e99ac92b3785788e62c7cae2242e11
Naelstrof <naelstrof@gmail.com>
2023-01-22 21:08:32 -0700

    optimized equipment networking.

--- Assets/KoboldKare/Scripts/Equipment/EquipmentDatabase.cs
+++ Assets/KoboldKare/Scripts/Equipment/EquipmentDatabase.cs
@@ -20,6 +20,9 @@ public class EquipmentDatabase : MonoBehaviour {
         foreach(var equipment in equipments) {
             equipmentDictionary.Add(equipment.name, equipment);
         }
+        if (equipmentDictionary.Count >= 256) {
+            throw new UnityException("Too many equipment! Only 256 unique equipment allowed...");
+        }
     }
     public static Equipment GetEquipment(string name) {
         if (GetInstance().equipmentDictionary.ContainsKey(name)) {
@@ -27,11 +30,11 @@ public class EquipmentDatabase : MonoBehaviour {
         }
         throw new UnityException("Failed to find equipment with name " + name);
     }
-    public static Equipment GetEquipment(short id) {
+    public static Equipment GetEquipment(byte id) {
         return GetInstance().equipments[id];
     }
-    public static short GetID(Equipment equipment) {
-        return (short)GetInstance().equipments.IndexOf(equipment);
+    public static byte GetID(Equipment equipment) {
+        return (byte)GetInstance().equipments.IndexOf(equipment);
     }
     public static List<Equipment> GetEquipments() => GetInstance().equipments;
     public List<Equipment> equipments;

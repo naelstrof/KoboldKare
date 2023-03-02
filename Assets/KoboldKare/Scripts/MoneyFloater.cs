40ce667eb054f54aa7ba4926881df4015c1fe84c
Naelstrof <naelstrof@gmail.com>
2023-03-01 21:25:04 -0700

    Increased memory budget for texture streaming.

--- Assets/KoboldKare/Scripts/MoneyFloater.cs
+++ Assets/KoboldKare/Scripts/MoneyFloater.cs
@@ -4,43 +4,31 @@ using UnityEngine;
 
 public class MoneyFloater : MonoBehaviour {
     [SerializeField]
-    private Transform textTransform;
-    [SerializeField]
     private TMPro.TMP_Text text;
-    private Coroutine routine;
     private int nextUpdate;
     private const float fadeDistance = 5f;
     public void SetBounds(Bounds target) {
-        textTransform.position = target.center;
-        textTransform.localScale = Vector3.one * target.size.magnitude;
+        transform.position = target.center;
+        transform.localScale = Vector3.one * target.size.magnitude;
     }
     public void SetText(string newText) {
         text.text = newText;
     }
     void OnEnable() {
-        routine = StartCoroutine(UpdateRoutine());
-    }
-    void OnDisable() {
-        if (routine != null) {
-            StopCoroutine(routine);
-        }
+        nextUpdate = 64;
+        StartCoroutine(UpdateRoutine());
     }
     IEnumerator UpdateRoutine() {
-        while(isActiveAndEnabled) {
+        while (isActiveAndEnabled) {
             // Every few frames we update
-            for(int i=0;i<nextUpdate;i++) {
+            for (int i = 0; i < nextUpdate; i++) {
                 yield return null;
             }
-            Camera check = Camera.main;
-            // Skip if camera is null
-            if (check == null) {
-                nextUpdate = 64;
-                continue;
-            }
-            float distance = textTransform.DistanceTo(check.transform);
-            textTransform.LookAt(check.transform, Vector3.up);
-            text.alpha = Mathf.Clamp01(fadeDistance-distance);
-            nextUpdate = distance < fadeDistance+1f ? 1 : 64;
+
+            float distance = Vector3.Distance(transform.position, OrbitCamera.GetPlayerIntendedPosition());
+            transform.LookAt(OrbitCamera.GetPlayerIntendedPosition(), Vector3.up);
+            text.alpha = Mathf.Clamp01(fadeDistance - distance);
+            nextUpdate = distance < fadeDistance + 1f ? 1 : 64;
         }
     }
 }

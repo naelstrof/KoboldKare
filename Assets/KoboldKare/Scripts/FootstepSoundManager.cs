f5f42dd5a1695dcc86b9bc929740e36d0c57db6b
Naelstrof <naelstrof@gmail.com>
2023-03-04 20:09:54 -0700

    Fixed footsteps having no volume or not triggering in some circumstances.

--- Assets/KoboldKare/Scripts/FootstepSoundManager.cs
+++ Assets/KoboldKare/Scripts/FootstepSoundManager.cs
@@ -28,12 +28,12 @@ public class FootstepSoundManager : MonoBehaviour {
         }
 
         Transform f = evt.intParameter == 0 ? animator.GetBoneTransform(HumanBodyBones.LeftFoot) : animator.GetBoneTransform(HumanBodyBones.RightFoot);
-        RaycastHit hit;
         AudioClip clip = footstepPack.GetClip();
         bool rayHit = false;
-        if (Physics.Raycast(f.position, Vector3.down, out hit, groundedAnimation? 0.8f: 5f, GameManager.instance.walkableGroundMask, QueryTriggerInteraction.Ignore)) {
+        if (Physics.Raycast(f.position, Vector3.down, out var hit, groundedAnimation? 0.8f: 5f, GameManager.instance.walkableGroundMask, QueryTriggerInteraction.Ignore)) {
             rayHit = true;
             if (hit.collider != null) {
+                Debug.DrawLine(hit.point, hit.point+hit.normal,Color.blue,10f);
                 TerrainAudio a = hit.collider.GetComponent<TerrainAudio>();
                 PhysicMaterial mat = hit.collider.sharedMaterial;
                 if (a != null) {
@@ -46,7 +46,7 @@ public class FootstepSoundManager : MonoBehaviour {
             }
         }
         if (evt.stringParameter == "Airborne" || rayHit) {
-            GameManager.instance.SpawnAudioClipInWorld(clip, f.position, evt.floatParameter*0.5f, GameManager.instance.soundEffectGroup);
+            GameManager.instance.SpawnAudioClipInWorld(clip, f.position, 0.8f, GameManager.instance.soundEffectGroup);
         }
     }
 }

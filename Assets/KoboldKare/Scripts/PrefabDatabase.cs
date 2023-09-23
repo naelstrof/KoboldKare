88624339d822cdd1e194bb3e01bfe66232430ae4
naelstrof <naelstrof@gmail.com>
2023-09-23 16:52:57 -0600

    Removed mod configurations to simplify modding

--- Assets/KoboldKare/Scripts/PrefabDatabase.cs
+++ Assets/KoboldKare/Scripts/PrefabDatabase.cs
@@ -22,37 +22,13 @@ public class PrefabDatabase : ScriptableObject {
 
     private event PrefabReferencesChangedAction prefabReferencesChanged;
     private List<PrefabReferenceInfo> validInfos;
-    
-    private const string JsonLocation = "modConfiguration.json";
-
-    private static string jsonFolder {
-        get {
-            var path = $"{Application.persistentDataPath}/defaultUser/";
-            if (SteamManager.Initialized) {
-                path = $"{Application.persistentDataPath}/{SteamUser.GetSteamID().ToString()}/";
-            }
-            return path;
-        }
-    }
-
-    private static string jsonLocation {
-        get {
-            var path = $"{jsonFolder}/{JsonLocation}";
-            if (SteamManager.Initialized) {
-                path = $"{jsonFolder}/{JsonLocation}";
-            }
-            return path;
-        }
-    }
 
     [System.Serializable]
     public class PrefabReferenceInfo {
-        private bool enabled;
         private string key;
         private GameObject prefab;
         private readonly PrefabDatabase parentDatabase;
-        public PrefabReferenceInfo(PrefabDatabase database, string primaryKey, GameObject newPrefab, bool enabled = true) {
-            this.enabled = enabled;
+        public PrefabReferenceInfo(PrefabDatabase database, string primaryKey, GameObject newPrefab) {
             key = primaryKey;
             prefab = newPrefab;
             parentDatabase = database;
@@ -68,27 +44,13 @@ public class PrefabDatabase : ScriptableObject {
         public GameObject GetPrefab() {
             return prefab;
         }
-
-        public bool GetEnabled() {
-            return enabled;
-        }
         public bool IsValid() {
-            return enabled && prefab != null;
+            return prefab != null;
         }
-
-        public void SetEnabled(bool newValue) {
-            enabled = newValue;
-            parentDatabase.prefabReferencesChanged?.Invoke(parentDatabase.GetPrefabReferenceInfos());
-        }
-        //public bool GetEnabled() {
-            //return enabled;
-        //}
         public void Save(JSONNode n) {
-            n["enabled"] = enabled;
             n["key"] = key;
         }
         public void Load(JSONNode n) {
-            enabled = n["enabled"];
             key =n["key"];
         }
     }
@@ -131,95 +93,6 @@ public class PrefabDatabase : ScriptableObject {
 
     private void OnEnable() {
         readOnlyPrefabReferenceInfos = prefabReferenceInfos.AsReadOnly();
-    }
-
-    public string SavePlayerConfiguration() {
-        JSONNode n = GetJsonConfiguration();
-        var rootNode = new JSONArray();
-        foreach (var info in prefabReferenceInfos) {
-            if (info.GetEnabled()) {
-                continue;
-            }
-            JSONNode node = JSONNode.Parse("{}");
-            info.Save(node);
-            rootNode.Add(node);
-        }
-
-        if (n.HasKey(name)) {
-            n.Remove(name);
-        }
-        n[name] = rootNode;
-        using FileStream fileWrite = File.Open(jsonLocation, FileMode.Truncate);
-        string writeString = n.ToString(2);
-        fileWrite.Write(Encoding.UTF8.GetBytes(writeString),0,writeString.Length);
-        fileWrite.Close();
-        return writeString;
-    }
-
-    public static JSONNode GetJsonConfiguration() {
-        if (!Directory.Exists(jsonFolder)) {
-            Directory.CreateDirectory(jsonFolder);
-        }
-        if (!File.Exists(jsonLocation)) {
-            using FileStream quickWrite = File.Open(jsonLocation, FileMode.CreateNew);
-            byte[] write = { (byte)'{', (byte)'}', (byte)'\n' };
-            quickWrite.Write(write, 0, write.Length);
-            quickWrite.Close();
-        }
-        using FileStream file = File.Open(jsonLocation, FileMode.Open);
-        byte[] b = new byte[file.Length];
-        file.Read(b,0,(int)file.Length);
-        file.Close();
-        JSONNode n;
-        try {
-            n = JSON.Parse(Encoding.UTF8.GetString(b));
-        } catch {
-            n = JSON.Parse("{}");
-        }
-        return n;
-    }
-
-    public void LoadPlayerConfiguration(string json) {
-        JSONNode n = JSON.Parse(json);
-        foreach (var node in n[name]) {
-            if (node.Value.IsNull) {
-                continue;
-            }
-            PrefabReferenceInfo info = new PrefabReferenceInfo(this, "", null);
-            info.Load(node);
-            AddPrefab(info);
-        }
-    }
-    public void LoadPlayerConfiguration() {
-        JSONNode n = GetJsonConfiguration();
-        foreach (var search in prefabReferenceInfos) {
-            search.SetEnabled(true);
-        }
-        
-        if (!n.HasKey(name)) {
-            prefabReferencesChanged?.Invoke(GetPrefabReferenceInfos());
-            return;
-        }
-
-        foreach (var node in n[name]) {
-            if (node.Value.IsNull) {
-                continue;
-            }
-            bool foundInfo = false;
-            PrefabReferenceInfo info = new PrefabReferenceInfo(this, "", null);
-            info.Load(node);
-            foreach (var search in prefabReferenceInfos) {
-                if (search.GetKey() != info.GetKey()) continue;
-                search.SetEnabled(info.GetEnabled());
-                foundInfo = true;
-                break;
-            }
-
-            if (!foundInfo) {
-                AddPrefab(info);
-            }
-        }
-        prefabReferencesChanged?.Invoke(GetPrefabReferenceInfos());
     }
 
     public void AddPrefab(PrefabReferenceInfo newInfo) {

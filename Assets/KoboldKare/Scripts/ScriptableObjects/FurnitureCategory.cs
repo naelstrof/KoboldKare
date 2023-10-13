c954d8f4e82e20bc740d1333cafa138aa0a6bef4
kimek <kony999@gmail.com>
2023-10-13 21:57:51 +0200

    Removed old version

--- Assets/KoboldKare/Scripts/ScriptableObjects/FurnitureCategory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "New Furniture Category", menuName = "Data/Furniture Category")]
-public class FurnitureCategory : ScriptableObject
-{
-    public string catName;
-    public List<Furniture> furniture;
-}

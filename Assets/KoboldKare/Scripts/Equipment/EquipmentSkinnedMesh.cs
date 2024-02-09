2d171f065d52dfe751ea693abb109ea7b7ff04b9
TwistedFlog <138166350+TwistedFlog@users.noreply.github.com>
2024-02-09 01:44:09 -0700

    Blend shape states from Kobold Skinned mesh to equipment Skinned mesh (#320)
    * Copy blend shape states from a Kobold Skinned Mesh to skinned mesh equipments
    
    Allows to copy blend shapes from Kobold Body Skinned Mesh to Skinned Mesh equipment by placing the paths to the Skinned Mesh equipments.
    Useful for clothing equipment mods

--- Assets/KoboldKare/Scripts/Equipment/EquipmentSkinnedMesh.cs
+++ Assets/KoboldKare/Scripts/Equipment/EquipmentSkinnedMesh.cs
@@ -5,64 +5,88 @@ using Naelstrof.Inflatable;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New SkinnedMeshEquipment", menuName = "Equipment/SkinnedMesh Equipment", order = 1)]
-public class EquipmentSkinnedMesh : Equipment {
-    [SerializeField]
-    private GameObject prefabContainingSkinnedMeshRenderers;
-    public override GameObject[] OnEquip(Kobold k, GameObject groundPrefab) {
-        base.OnEquip(k, groundPrefab);
-        SkinnedMeshRenderer targetSkinnedMesh = k.koboldBodyRenderers[0] as SkinnedMeshRenderer;
-        GameObject instance = Instantiate(prefabContainingSkinnedMeshRenderers, k.transform);
-        JiggleSkin jiggleSkin = k.GetComponent<JiggleSkin>();
-        instance.name = name;
-        foreach(var skinnedMesh in instance.GetComponentsInChildren<SkinnedMeshRenderer>()) {
-            Transform[] newBoneList = new Transform[targetSkinnedMesh.bones.Length];
-            for (int i = 0; i < targetSkinnedMesh.bones.Length; i++) {
-                newBoneList[i] = targetSkinnedMesh.bones[i];
-            }
-            skinnedMesh.bones = newBoneList;
-            skinnedMesh.rootBone = targetSkinnedMesh.rootBone;
-            k.koboldBodyRenderers.Add(skinnedMesh);
-            foreach (var inflater in k.GetAllInflatableListeners()) {
-                if (inflater is InflatableBreast inflatableBreast) {
-                    inflatableBreast.AddTargetRenderer(skinnedMesh);
-                }
-                if (inflater is InflatableBelly belly) {
-                    belly.AddTargetRenderer(skinnedMesh);
-                }
-                if (inflater is InflatableBlendShape inflatableBlendShape) {
-                   inflatableBlendShape.AddTargetRenderer(skinnedMesh);
-                }
-            }
-
-            if (jiggleSkin != null) {
-                jiggleSkin.targetSkins.Add(skinnedMesh);
-            }
-        }
-        return new[] { instance };
+public class EquipmentSkinnedMesh: Equipment {
+  [SerializeField]
+  private GameObject prefabContainingSkinnedMeshRenderers;
+  private List < SkinnedMeshRenderer > targets;
+  private class BlendShapeCopier: MonoBehaviour {
+    public SkinnedMeshRenderer source;
+    private List < SkinnedMeshRenderer > targets;
+    void Awake() {
+      targets = new List < SkinnedMeshRenderer > (GetComponentsInChildren < SkinnedMeshRenderer > ());
     }
-
-    public override GameObject OnUnequip(Kobold k, bool dropOnGround = true) {
-        JiggleSkin jiggleSkin = k.GetComponent<JiggleSkin>();
-        Transform search = k.transform.Find(name);
-        if (search == null) return base.OnUnequip(k, dropOnGround);
-        foreach (var skinnedMesh in search.GetComponentsInChildren<SkinnedMeshRenderer>()) {
-            if (jiggleSkin != null) {
-                jiggleSkin.targetSkins.Remove(skinnedMesh);
-            }
-            foreach (var inflater in k.GetAllInflatableListeners()) {
-                if (inflater is InflatableBreast inflatableBreast) {
-                    inflatableBreast.RemoveTargetRenderer(skinnedMesh);
-                }
-                if (inflater is InflatableBelly belly) {
-                    belly.RemoveTargetRenderer(skinnedMesh);
-                }
-                if (inflater is InflatableBlendShape inflatableBlendShape) {
-                   inflatableBlendShape.RemoveTargetRenderer(skinnedMesh);
-                }
-            }
-            k.koboldBodyRenderers.Remove(skinnedMesh);
+    void Update() {
+      foreach(var target in targets) {
+        CopyBlendShapes(target);
+      }
+    }
+    void CopyBlendShapes(SkinnedMeshRenderer target) {
+      int blendShapeCount = source.sharedMesh.blendShapeCount;
+      for (int i = 0; i < blendShapeCount; i++) {
+        string blendShapeName = source.sharedMesh.GetBlendShapeName(i);
+        int sourceBlendShapeIndex = source.sharedMesh.GetBlendShapeIndex(blendShapeName);
+        int targetBlendShapeIndex = target.sharedMesh.GetBlendShapeIndex(blendShapeName);
+        if (sourceBlendShapeIndex != -1 && targetBlendShapeIndex != -1) {
+          float weight = source.GetBlendShapeWeight(sourceBlendShapeIndex);
+          target.SetBlendShapeWeight(targetBlendShapeIndex, weight);
         }
-        Destroy(search.gameObject);
-        return base.OnUnequip(k, dropOnGround);
+      }
+    }
+  }
+  public override GameObject[] OnEquip(Kobold k, GameObject groundPrefab) {
+    base.OnEquip(k, groundPrefab);
+    SkinnedMeshRenderer targetSkinnedMesh = k.koboldBodyRenderers[0] as SkinnedMeshRenderer;
+    GameObject instance = Instantiate(prefabContainingSkinnedMeshRenderers, k.transform);
+    JiggleSkin jiggleSkin = k.GetComponent < JiggleSkin > ();
+    instance.name = name;
+    BlendShapeCopier copier = instance.AddComponent < BlendShapeCopier > ();
+    copier.source = targetSkinnedMesh;
+    foreach(var skinnedMesh in instance.GetComponentsInChildren < SkinnedMeshRenderer > ()) {
+      Transform[] newBoneList = new Transform[targetSkinnedMesh.bones.Length];
+      for (int i = 0; i < targetSkinnedMesh.bones.Length; i++) {
+        newBoneList[i] = targetSkinnedMesh.bones[i];
+      }
+      skinnedMesh.bones = newBoneList;
+      skinnedMesh.rootBone = targetSkinnedMesh.rootBone;
+      k.koboldBodyRenderers.Add(skinnedMesh);
+      foreach(var inflater in k.GetAllInflatableListeners()) {
+        if (inflater is InflatableBreast inflatableBreast) {
+          inflatableBreast.AddTargetRenderer(skinnedMesh);
+        }
+        if (inflater is InflatableBelly belly) {
+          belly.AddTargetRenderer(skinnedMesh);
+        }
+        if (inflater is InflatableBlendShape inflatableBlendShape) {
+          inflatableBlendShape.AddTargetRenderer(skinnedMesh);
+        }
+      }
+      if (jiggleSkin != null) {
+        jiggleSkin.targetSkins.Add(skinnedMesh);
+      }
+    }
+    return new [] {
+      instance
+    };
+  }
+  public override GameObject OnUnequip(Kobold k, bool dropOnGround = true) {
+    JiggleSkin jiggleSkin = k.GetComponent < JiggleSkin > ();
+    Transform search = k.transform.Find(name);
+    if (search == null) return base.OnUnequip(k, dropOnGround);
+    foreach(var skinnedMesh in search.GetComponentsInChildren < SkinnedMeshRenderer > ()) {
+      if (jiggleSkin != null) {
+        jiggleSkin.targetSkins.Remove(skinnedMesh);
+      }
+      foreach(var inflater in k.GetAllInflatableListeners()) {
+        if (inflater is InflatableBreast inflatableBreast) {
+          inflatableBreast.RemoveTargetRenderer(skinnedMesh);
+        }
+        if (inflater is InflatableBelly belly) {
+          belly.RemoveTargetRenderer(skinnedMesh);
+        }
+        if (inflater is InflatableBlendShape inflatableBlendShape) {
+          inflatableBlendShape.RemoveTargetRenderer(skinnedMesh);
+        }
+      }
+      k.koboldBodyRenderers.Remove(skinnedMesh);
     }
 }

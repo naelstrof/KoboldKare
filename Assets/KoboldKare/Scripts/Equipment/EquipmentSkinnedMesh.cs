3bd7ad18d4d39299c01b5af24548986911a31d38
JonBax <127814381+JonBax@users.noreply.github.com>
2024-02-25 16:46:53 -0300

    Add blend shapes to skinned mesh equipment
    Allows a list of blend shapes to be applied to a kobold when skinned mesh equips are added to them.
    
    Does not play nicely with constantly changing shapes such as inflation related ones, as you'd expect.

--- Assets/KoboldKare/Scripts/Equipment/EquipmentSkinnedMesh.cs
+++ Assets/KoboldKare/Scripts/Equipment/EquipmentSkinnedMesh.cs
@@ -1,19 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using JigglePhysics;
 using Naelstrof.Inflatable;
+using PenetrationTech;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New SkinnedMeshEquipment", menuName = "Equipment/SkinnedMesh Equipment", order = 1)]
 public class EquipmentSkinnedMesh : Equipment {
     [SerializeField]
     private GameObject prefabContainingSkinnedMeshRenderers;
+    [SerializeField]
+    private SkinnedMeshTweak[] blendShapesToTweak;
     public override GameObject[] OnEquip(Kobold k, GameObject groundPrefab) {
         base.OnEquip(k, groundPrefab);
         SkinnedMeshRenderer targetSkinnedMesh = k.koboldBodyRenderers[0] as SkinnedMeshRenderer;
         GameObject instance = Instantiate(prefabContainingSkinnedMeshRenderers, k.transform);
         JiggleSkin jiggleSkin = k.GetComponent<JiggleSkin>();
         instance.name = name;
+        // Tweak - change blend shapes on equip
+        foreach (SkinnedMeshTweak blend in blendShapesToTweak)
+        {
+            foreach (SkinnedMeshRenderer mesh in k.koboldBodyRenderers)
+            {
+                Mesh m = mesh.sharedMesh;
+                int index = m.GetBlendShapeIndex(blend.blendShapeName);
+                if(index == -1) continue;
+                blend.originalShapeValue = mesh.GetBlendShapeWeight(index);
+                mesh.SetBlendShapeWeight(index, blend.shapeValue);
+            }
+        }
+        // - - - - - - - - - - - - 
         foreach(var skinnedMesh in instance.GetComponentsInChildren<SkinnedMeshRenderer>()) {
             Transform[] newBoneList = new Transform[targetSkinnedMesh.bones.Length];
             for (int i = 0; i < targetSkinnedMesh.bones.Length; i++) {
@@ -62,7 +80,30 @@ public class EquipmentSkinnedMesh : Equipment {
             }
             k.koboldBodyRenderers.Remove(skinnedMesh);
         }
+        // Tweak - reset blend shapes on unequip
+        foreach (SkinnedMeshTweak blend in blendShapesToTweak)
+        {
+            foreach (SkinnedMeshRenderer mesh in k.koboldBodyRenderers)
+            {
+                Mesh m = mesh.sharedMesh;
+                int index = m.GetBlendShapeIndex(blend.blendShapeName);
+                if(index == -1) continue;
+                mesh.SetBlendShapeWeight(index, blend.originalShapeValue);
+            }
+        }
+        // - - - - - - - - - - - - 
         Destroy(search.gameObject);
         return base.OnUnequip(k, dropOnGround);
     }
+}
+
+[System.Serializable]
+public class SkinnedMeshTweak
+{
+    [SerializeField]
+    public string blendShapeName;
+    [SerializeField] [Range(0.0f, 100.0f)]
+    public float shapeValue;
+    [NonSerialized] 
+    public float originalShapeValue = 0;
 }

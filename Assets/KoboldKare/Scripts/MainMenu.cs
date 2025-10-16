a603745ee598796c80759cff385150affc38fd93
naelstrof <naelstrof@gmail.com>
2025-10-16 15:14:46 -0600

    Added some debug messaging, added clothing hue localization.

--- Assets/KoboldKare/Scripts/MainMenu.cs
+++ Assets/KoboldKare/Scripts/MainMenu.cs
@@ -82,7 +82,6 @@ public class MainMenu : MonoBehaviour {
     public static MainMenuMode GetCurrentMode() => instance.currentMode;
 
     public void ShowMenu(MainMenuMode mode) {
-        Debug.Log("Switching to menu mode " + mode);
         MultiplayerTab.SetActive(false);
         OptionsTab.SetActive(false);
         MainViewTab.SetActive(false);
--- Assets/KoboldKare/Scripts/Utility/VersionNumberUpdater.cs
+++ Assets/KoboldKare/Scripts/Utility/VersionNumberUpdater.cs
@@ -4,5 +4,6 @@ using TMPro;
 public class VersionNumberUpdater : MonoBehaviour{
     void Start(){
         GetComponent<TextMeshProUGUI>().text = Application.version.ToString();
+        Debug.Log("KK Version: " + Application.version.ToString());
     }
 }

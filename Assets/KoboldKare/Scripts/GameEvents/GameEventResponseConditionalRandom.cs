cf6707a695f1ddb68a563845479330b6e8c7ffcb
Amalien <159964271+Amalite@users.noreply.github.com>
2025-11-24 02:10:12 -0500

    Added ConditionalRandom
    Rolls a random number between Min and Max, if the Roll is higher than the Minimum Requirement, invokes Responses.

---
+++ Assets/KoboldKare/Scripts/GameEvents/GameEventResponseConditionalRandom.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameEventResponseConditionalRandom : GameEventResponse
+{
+    public int Min;
+    public int Max;
+    public int MininumRequirement;
+    [SerializeField, SubclassSelector, SerializeReference] private List<GameEventResponse> requirementMet = new List<GameEventResponse>();
+    private int Roll;
+
+    public override void Invoke(MonoBehaviour owner)
+    {
+        Roll = UnityEngine.Random.Range(Min, Max);
+        Debug.Log(Roll);
+        if (Roll >= MininumRequirement)
+        {
+            foreach (var response in requirementMet)
+            {
+                response?.Invoke(owner);
+            }
+        }
+    }
+}

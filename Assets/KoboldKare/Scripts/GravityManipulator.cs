f4d283e7adf389661f23a7822d28a86a13753fef
Amalien <159964271+Amalite@users.noreply.github.com>
2025-12-06 02:06:35 -0500

    Turn GravityManipulator Gravity into a float that exclusively controls downwards gravity.
    A simple solution to prevent the weird floor detection with non-downwards gravity is to simply make it harder to be non-downwards.

--- Assets/KoboldKare/Scripts/GravityManipulator.cs
+++ Assets/KoboldKare/Scripts/GravityManipulator.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class GravityManipulator : MonoBehaviour
 {
-    public Vector3 Gravity = new(0f,-9.81f,0f);
-
+    [Range(-200f, 0f)]
+    public float Gravity = -9.81f;
     private void Start()
     {
-        Physics.gravity = Gravity;
-        Physics.clothGravity = Gravity;
+
+        Physics.gravity = new(0f, Gravity, 0f);
+        Physics.clothGravity = new(0f, Gravity, 0f);
     }
 }
